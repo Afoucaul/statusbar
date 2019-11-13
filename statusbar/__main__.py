e06443f79246d2f70a1cbc2fa20d9843ad6375f8
Armand Foucault <armand@cardiologs.com>
2019-11-14 00:25:31 +0100

    Add volume to sample bar

--- statusbar/__main__.py
+++ statusbar/__main__.py
@@ -4,11 +4,13 @@ from statusbar import bar as statusbar
 
 fmt = " " + " | ".join((
     "{bat0}",
-    "{bat1}"
+    "{bat1}",
+    "{vol}"
 )) + " "
 components = {
     'bat0': components.Battery(1, 0, "M:[{energy}]{status}"),
-    'bat1': components.Battery(1, 1, "M:[{energy}]{status}")
+    'bat1': components.Battery(1, 1, "M:[{energy}]{status}"),
+    'vol': components.Volume("vol: {volume:.0%}")
 }
 
 bar = statusbar.StatusBar(fmt, components, print)
