fa0ef72d9eeee22f33e00e1c7e338c30de80fce9
Armand Foucault <armand@cardiologs.com>
2019-11-14 11:25:44 +0100

    Package module

---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+
+setuptools.setup(
+    author="Armand Foucault",
+    long_description=open("README.md").read(),
+    name="statusbar",
+    packages=setuptools.find_packages(),
+)
--- statusbar/__main__.py
+++ statusbar/__main__.py
@@ -4,24 +4,29 @@ from statusbar import utils
 from statusbar import weathercli
 
 
-fmt = " " + " | ".join((
-    "{bat0}",
-    "{bat1}",
-    "{vol}",
-    "{wifi}",
-    "{weather}",
-    "{datetime}",
-)) + " "
+def main():
+    fmt = " " + " | ".join((
+        "{bat0}",
+        "{bat1}",
+        "{vol}",
+        "{wifi}",
+        "{weather}",
+        "{datetime}",
+    )) + " "
 
-session = weathercli.Session.from_env()
-components = {
-    'bat0': components.Battery("M:[{energy}]{status}", 0),
-    'bat1': components.Battery("S:[{energy}]{status}", 1),
-    'vol': components.Volume("vol: {volume:.0%}"),
-    'wifi': components.WiFi("{essid}: {power}", "wlp3s0"),
-    'datetime': components.DateTime("{date} - {time}"),
-    'weather': components.Weather("{city} - {temp}°, {condition}", session),
-}
+    session = weathercli.Session.from_env()
+    parts = {
+        'bat0': components.Battery("M:[{energy}]{status}", 0),
+        'bat1': components.Battery("S:[{energy}]{status}", 1),
+        'vol': components.Volume("vol: {volume:.0%}"),
+        'wifi': components.WiFi("{essid}: {power}", "wlp3s0"),
+        'datetime': components.DateTime("{date} - {time}"),
+        'weather': components.Weather("{city} - {temp}°, {condition}", session),
+    }
 
-bar = statusbar.StatusBar(fmt, components, utils.xsetroot_name)
-bar.start()
+    bar = statusbar.StatusBar(fmt, parts, utils.xsetroot_name)
+    bar.start()
+
+
+if __name__ == '__main__':
+    main()
