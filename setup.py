20f481eb80b2bfcec39eda9f87150badc43e9d43
Armand Foucault <armand.foucault@telecom-bretagne.eu>
2020-11-25 16:40:15 +0100

    Add requirements to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 import setuptools
 
+with open("./requirements.txt", "r") as fd:
+    requirements = list(fd)
+
 
 setuptools.setup(
     author="Armand Foucault",
     long_description=open("README.md").read(),
     name="statusbar",
     packages=setuptools.find_packages(),
+    install_requires=requirements
 )
